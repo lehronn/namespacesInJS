82050d3839fdd0f14657b9f105a7b365d4892055
Mateusz Stomski <lehronn@gmail.com>
2018-02-06 19:48:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Good approach for namespaces in JavaScript.
+
+## Others informations
+This example is based on Piotr "shpyo" Cichosz`s post.
+
+You can read this post here: http://www.jportal.pl/namespace-w-js.html
---
+++ index.js
@@ -0,0 +1,24 @@
+//namespaces in JavaScript.
+let sk = {
+  main : {
+    seyHello : function() {...};,
+    checkBrowser : function() {...};,
+    checkIp : function() {...};
+  },
+  about : {
+    writeText : function() {...};,
+    alertStatus : function() {...};
+  },
+  contact : {
+    checkEmail : function() {...};,
+    checkStatus : function() {...};
+  },
+  form : {
+    validateDate : function() {...};,
+    validateAuthor : function() {...};,
+    validateEmail : function() {...};
+  }
+}
+
+sk.main.seyHello(username);
+sk.contact.checkEmail();
